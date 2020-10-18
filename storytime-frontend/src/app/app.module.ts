2087d62c209b8654c2fcfe0a4c06aeaf6c96b464
Henry Post <henryfbp@gmail.com>
2020-10-18 09:26:00 -0500

    add stories component

--- storytime-frontend/src/app/app.module.ts
+++ storytime-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoriesComponent } from './stories/stories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule,
