d6cc4e1483d5fb8f5a98d88e95258b4f537a60c8
Henry Post <henryfbp@gmail.com>
2020-10-18 10:07:16 -0500

    make story interface and display object props in html

--- storytime-frontend/src/app/stories/stories.component.ts
+++ storytime-frontend/src/app/stories/stories.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Story } from '../story';
 
 @Component({
   selector: 'app-stories',
   templateUrl: './stories.component.html',
-  styleUrls: ['./stories.component.sass']
+  styleUrls: ['./stories.component.sass'],
 })
 export class StoriesComponent implements OnInit {
+  story: Story = {
+    id: 0,
+    title: "Best Story Ever",
+    textContent: 'Once upon a time...',
+    tags: ['fantasy', 'adventure'],
+  };
 
-  story = 'A really great story';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ storytime-frontend/src/app/story.ts
@@ -0,0 +1,6 @@
+export interface Story {
+    id: Number;
+    title: String;
+    textContent: String;
+    tags?: Array<String>;
+}
