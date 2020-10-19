3ca41bba9cd01eed4a89e8b71f5255c2b5eb2f9a
Henry Post <henryfbp@gmail.com>
2020-10-19 16:21:08 -0400

    generate StoryDetailComponent

--- storytime-frontend/src/app/app.module.ts
+++ storytime-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoriesComponent } from './stories/stories.component';
+import { StoryDetailComponent } from './story-detail/story-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StoriesComponent
+    StoriesComponent,
+    StoryDetailComponent
   ],
   imports: [
     BrowserModule,
