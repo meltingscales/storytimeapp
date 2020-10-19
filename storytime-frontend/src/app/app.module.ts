8173ecef4ef12bbd9c73f36d4e0e993aca5b26e7
Henry Post <henryfbp@gmail.com>
2020-10-19 17:20:05 -0400

    add Messages component

--- storytime-frontend/src/app/app.module.ts
+++ storytime-frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoriesComponent } from './stories/stories.component';
 import { StoryDetailComponent } from './story-detail/story-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StoriesComponent,
-    StoryDetailComponent
+    StoryDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
