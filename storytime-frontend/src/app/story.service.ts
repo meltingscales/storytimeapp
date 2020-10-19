29f666fbe4ef5b8f1b1591e675655ea2f3b63ab4
Henry Post <henryfbp@gmail.com>
2020-10-19 18:16:22 -0400

    use message service in StoryService

--- storytime-frontend/src/app/story.service.ts
+++ storytime-frontend/src/app/story.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Story } from './story';
+import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
 import { STORIES } from './mock-stories';
 
@@ -7,9 +8,10 @@ import { STORIES } from './mock-stories';
   providedIn: 'root',
 })
 export class StoryService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getStories(): Observable<Story[]> {
+    this.messageService.add('StoryService: Fetched Stories.');
     return of(STORIES);
   }
 }
